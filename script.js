313f75722628804b0a83dfa4fee31348ac9b0827
AlissCava <cavalleraalissa@gmail.com>
2023-12-04 15:26:22 +0100

    bozze css e js + immagini

---
+++ script.js
@@ -0,0 +1,13 @@
+// Array di oggetti letterali con le informazioni sugli slide
+
+// Elementi HTML
+
+// Indice dell'immagine attualmente visualizzata
+
+// Funzione per aggiornare il carosello con i dati dell'immagine corrente
+
+// Funzione per mostrare lo slide successivo
+
+// Gestori degli eventi per i pulsanti di navigazione
+
+// Inizializzazione del carosello con la prima immagine
